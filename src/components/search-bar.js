9f7607261fe2d7f87697ad024f0d6212e5030189
Rooarii <rooarii.manuel@gmail.com>
2019-07-04 10:30:16 +0200

    Set up the project architecture + s SearchBox component

---
+++ src/components/search-bar.js
@@ -0,0 +1,16 @@
+import React,{Component} from 'react'
+/*
+const SearchBar = function(){
+    return <input/>
+}
+
+const SearchBar = ()=>{
+    return <input/>
+}*/
+
+class SearchBar extends Component{
+    render(){
+        return<input/>
+    }
+}
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
+import App from './containers/app'
 const API_KEY ="184a078836bbb9d9a754e937f43861a4";
-
-const App = function (){
-    return <div>Salut les amis!</div>
-}
 
 ReactDOM.render(<App/>,document.querySelector('.container'));
