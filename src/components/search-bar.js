dd223d6c763c2b4007a2f109662ec780194a0665
Rooarii <rooarii.manuel@gmail.com>
2019-08-11 15:09:44 +0200

    CSS boostrap added for video list item

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -15,8 +15,10 @@ class SearchBar extends Component{
     }
     render(){
         return (
-            <div>
-                <input onChange={this.handleChange.bind(this)} placeholder ={this.state.placeHolder}/>
+            <div className="row">
+                <div className="col-md-8">
+                    <input type="text" className="form-control" onChange={this.handleChange.bind(this)} placeholder ={this.state.placeHolder}/>
+                </div>
             </div>   
         )
     }
--- src/components/video-list-item.js
+++ src/components/video-list-item.js
@@ -2,9 +2,15 @@ import React from 'react'
 
 const IMAGE_BASE_URL= "https://image.tmdb.org/t/p/w500/";
 const VideoListItem = ({movie}) =>{
-    return <li>
-        <img heght = "100px" width ="100px" src= {`${IMAGE_BASE_URL}${movie.poster_path}`}/>
-        <h1>{movie.title}</h1>
+    return <li className="list-group-item">
+        <div className="media">
+            <div className="media-left">
+                <img className="media-object img-rounded" heght = "100px" width ="100px" src= {`${IMAGE_BASE_URL}${movie.poster_path}`}/>
+            </div>
+            <div className="media-body">
+                <h5 className="title-list-item">{movie.title}</h5>
+            </div>
+        </div>
     </li>
 }
 
